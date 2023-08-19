83525bd4d8ed793fc477844ba0e1cfb33dcefb28
Hellentorresr <htorres1@ucenfotec.ac.cr>
2023-08-18 21:07:01 -0600

    Adding a parameter to the GetDepartments method to accept a list of employees

--- LINQ_course/TCPData/Department.cs
+++ LINQ_course/TCPData/Department.cs
@@ -11,5 +11,8 @@ namespace TCPData
         public int Id { get; set; }
         public string ShortName { get; set; }
         public string LongName { get; set; }
+
+        //now each department will stored a collection of related of employee records
+        public IEnumerable<Employee> Employees { get; set; }
     }
 }
